5deaefe541190b09ddb826d06025ba77051a0545
Shifa <shifa.hayat1@ucalgary.ca>
2020-12-01 12:40:39 -0700

    Design

---
+++ js/Images.js
@@ -0,0 +1,39 @@
+var Fauble = new L.icon ({
+	  iconUrl: 'https://pbs.twimg.com/media/EGi38n7UYAAcZYI?format=jpg&name=small',
+	  iconSize: [70, 70]
+	  });
+
+var Kipchoge = new L.icon ({
+	  iconUrl: 'https://static01.nyt.com/images/2019/04/28/world/28london-marathon1/28london-marathon1-superJumbo.jpg?quality=90&auto=webp',
+	  iconSize: [71, 47]
+	  });
+
+var Tuliamuk = new L.icon ({
+	  iconUrl: 'https://d275froxpp5hf7.cloudfront.net/wp-content/uploads/2020/03/Screen-Shot-2020-03-05-at-8.10.14-PM.png',
+	  iconSize: [70,70]
+	  });
+
+var Ferrell = new L.icon ({
+	  iconUrl: 'https://www2.pictures.zimbio.com/pc/Will+Ferrell+seen+running+while+filming+scenes+VmifSpzIiPCx.jpg',
+	  iconSize: [54,70]
+	  });
+
+var Oprah = new L.icon ({
+	  iconUrl: 'https://media.nbcchicago.com/2019/09/oprah-marathoner-celebrity.jpg?fit=1396%2C2021',
+	  iconSize: [48,70]
+	  });
+
+var Whitlock = new L.icon ({
+	  iconUrl: 'https://i.cbc.ca/1.4023108.1489434614!/fileImage/httpImage/image.jpeg_gen/derivatives/16x9_780/ed-whitlock.jpeg',
+	  iconSize: [70,47]
+	  });
+
+var Wozniacki = new L.icon ({
+	  iconUrl: 'https://s.wsj.net/public/resources/images/BN-FI727_hof110_G_20141103155601.jpg',
+	  iconSize: [70, 47]
+      });
+
+var Bruschi = new L.icon ({
+	  iconUrl: 'https://chowderandchampions.com/wp-content/uploads/getty-images/2017/11/79137007-afc-championship-san-diego-chargers-v-new-england-patriots.jpg.jpg',
+      iconSize: [70, 47]
+      });
